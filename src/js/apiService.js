f028daedbba0a265cbdb024eebd356a928295d20
Tanya Sabadyshyna <sabadyshyna@gmail.com>
2020-11-16 15:06:47 +0200

    add hw

---
+++ src/js/apiService.js
@@ -0,0 +1,32 @@
+export default class ImageApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImages() {
+    const API_KEY = '19086013-8913d7edb8f4a3e99ae0d2d02';
+    const BASE_URL = 'https://pixabay.com/api';
+
+    const response = await fetch(
+      `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+    );
+
+    const newImages = await response.json();
+
+    this.page += 1;
+    return newImages.hits;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
